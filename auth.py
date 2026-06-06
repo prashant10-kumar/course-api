c3d967e1d07f4f9b1aaa515fe6ffd24db2c8f3d3
prashant10-kumar <prashantk4644@gmail.com>
2026-06-07 02:16:16 +0530

    "Add password verificaton and set variable enviorment"

---
+++ auth.py
@@ -0,0 +1,37 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+import os
+import bcrypt
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def hash_password(password : str)->str:
+    return bcrypt.hashpw(
+        password.encode("utf-8"),
+        bcrypt.gensalt()
+    ).decode("utf-8")
+
+def verify_password(plain_password: str, hashed_password):
+    return bcrypt.hashpw(
+        plain_password.encode("utf-8"),
+        hashed_password.encode("utf-8")
+    )
+
+def create_access_token(data: dict)->str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp" : expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM)
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algoriths = [ALGORITHM])
+        username: str = payload.get("sub")
+        if username is none:
+            return None
+        return username
+    except JWTError:
+        return None
